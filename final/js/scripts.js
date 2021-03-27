95678431c50a080d5c230074da93727ae75f4343
dellborg82 <dellborg82@byui.edu>
2021-03-27 04:42:16 -0600

    More work on final Project

---
+++ final/js/scripts.js
@@ -0,0 +1,47 @@
+function toggleMenu() {
+  document.getElementById("primaryNav").classList.toggle("hide")
+}
+
+function getFullYear() {
+  document.write(new Date().getFullYear());
+}
+
+function lastUpdated() {
+  document.getElementById("lastupdated").textContent = "Last Updated: " + document.lastModified;
+}
+
+function getCurrentDate() {
+  const dayNames = [
+    "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+];
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+  const todaysDate = new Date();
+  const dayOfWeek = dayNames[todaysDate.getDay()];
+  const monthName = months[todaysDate.getMonth()];
+  const year = todaysDate.getFullYear();
+  const currentDate = dayOfWeek + " " + " " + monthName + " " + todaysDate.getDate() + ", " + year;
+  const options = {weekday: 'long', month: 'long', day: 'numeric', year: 'numeric'};
+  document.getElementById("currentDate").textContent = currentDate;
+}
+
+
+function font() {
+WebFont.load({
+  google: {
+    families: ["Darker Grotesque", "cursive"],
+  },
+});
+}
