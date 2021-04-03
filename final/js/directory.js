8bcd74ad92c7f515784feb1b507ffe716f69c9f5
dellborg82 <dellborg82@byui.edu>
2021-04-03 15:28:24 -0600

    Directory Page is up, but needs formatting.

---
+++ final/js/directory.js
@@ -0,0 +1,46 @@
+const requestURL = "/final/json/business.json";
+
+fetch(requestURL)
+.then(function (response) {
+   return response.json();
+})
+.then(function (jsonObject) {
+  const business = jsonObject['business'];
+  for (let i = 0; i < business.length; i++) {
+      
+        let card = document.createElement('section');
+        let name = document.createElement('h2');
+        let image = document.createElement('img');
+        let type = document.createElement('p');
+        let website = document.createElement('p');
+        let yearFounded = document.createElement('p');
+        let details = document.createElement('div');
+      
+        name.textContent = business[i].name;
+        
+        
+        name.textContent = 'Business Name: ' + business[i].name;
+        website.textContent = 'Website: ' + business[i].Website;
+        type.textContent = 'Business info: ' + business[i].type;
+        yearFounded.textContent = 'Year Founded: ' + business[i].yearFounded;
+        
+        image.setAttribute('src', 'images/logos/' + business[i].photo);
+        image.setAttribute('alt', business[i].name);
+        
+        details.setAttribute('id', 'details');
+        
+        card.appendChild(details);
+        card.appendChild(image);
+        
+        details.appendChild(name);
+        details.appendChild(type);
+        
+        details.appendChild(website);
+        details.appendChild(type);
+
+        details.appendChild(yearFounded);
+        document.querySelector('div.cards').appendChild(card);
+      
+   }
+
+  });
